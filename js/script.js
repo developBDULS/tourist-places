7952f478fce5b85c4a30a790f08f8f3b716bcf9a
developBDULS <desarrollo.bigdatauls@gmail.com>
2023-02-15 10:17:51 -0300

    add info sider

--- js/script.js
+++ js/script.js
@@ -9,15 +9,15 @@ const map = new mapboxgl.Map({
 });
 
 map.on('load', ()=>{
-    d3.json("data/places.json", function (p) {
+    map.loadImage(
+        'media/mapbox-marker-icon-20px-red.png',
+        (error, image) => {
+            if(error) throw error;
+            map.addImage('maker-red', image);
+        }
+    );
 
-        map.loadImage(
-            'media/mapbox-marker-icon-20px-red.png',
-            (error, image) => {
-                if(error) throw error;
-                map.addImage('maker-blue', image);
-            }
-        );
+    d3.json("data/places.json", function (p) {        
         places = p;
         //add source
         map.addSource('places',{
@@ -30,9 +30,34 @@ map.on('load', ()=>{
             'type': 'symbol',
             'source': 'places',
             'layout': {
-                'icon-image': 'maker-blue',
+                'icon-image': 'maker-red',
                 'icon-size': 0.75
             }
         });
     });
+});
+
+map.on('click', 'points',(e)=>{
+    console.log('works! :)')
+    oc = document.getElementById('offcanvasExample')
+    let bsoffcanvas = new bootstrap.Offcanvas(oc)
+    bsoffcanvas.show()
+    
+    let data_commune = e.features[0].properties.commune;
+    let data_name = e.features[0].properties.name;
+    let data_category = e.features[0].properties.category;
+    let data_location = e.features[0].properties.location;
+    let data_address = e.features[0].properties.address;
+
+    let tag_commune = document.getElementById('commune')
+    let tag_name =  document.getElementById('name')
+    let tag_category = document.getElementById('category')
+    let tag_location = document.getElementById('location')
+    let tag_address = document.getElementById('address')    
+
+    tag_name.innerHTML = "<b style='font-size:20px'>"+data_name+"</b>";
+    tag_commune.innerHTML = "<b>Comuna: </b>"+data_commune;
+    tag_category.innerHTML = "<b>Categoría: </b>"+data_category;
+    tag_location.innerHTML = "<b>Localidad:</b> "+data_location;
+    tag_address.innerHTML = "<b>Dirección:</b> "+data_address; 
 });

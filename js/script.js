9c1de4e804f22f31e5f1f1b842a5ed3340a7dae1
developBDULS <desarrollo.bigdatauls@gmail.com>
2023-02-15 08:26:41 -0300

    add media and data

--- js/script.js
+++ js/script.js
@@ -5,5 +5,34 @@ const map = new mapboxgl.Map({
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
     center: [-71.24976442905432, -29.90303496912085], // starting position [lng, lat]
-    zoom: 15.773 // starting zoom
+    zoom: 7 // starting zoom
+});
+
+map.on('load', ()=>{
+    d3.json("data/places.json", function (p) {
+
+        map.loadImage(
+            'media/mapbox-marker-icon-20px-red.png',
+            (error, image) => {
+                if(error) throw error;
+                map.addImage('maker-blue', image);
+            }
+        );
+        places = p;
+        //add source
+        map.addSource('places',{
+            'type': 'geojson',
+            'data': places
+        });        
+        //addLayer
+        map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'places',
+            'layout': {
+                'icon-image': 'maker-blue',
+                'icon-size': 0.75
+            }
+        });
+    });
 });
